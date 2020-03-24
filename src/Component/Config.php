dabd1e19955cca75c55a61db4151b4da789ca703
Arnaud BODEL <contact@osw3.net>
2020-03-25 00:57:07 +0100

    -

---
+++ src/Component/Config.php
@@ -0,0 +1,52 @@
+<?php
+namespace Dragon\Component;
+
+class Config
+{
+    const DIRECTORY = \Dragon\Core::DIRECTORY_APP . "config/";
+
+    /**
+     * The App config
+     *
+     * @var array
+     */
+    private $config = [];
+
+    public function __construct()
+    {
+		// Config files
+		$config_file = self::DIRECTORY . "config.php";
+		$config_dev  = self::DIRECTORY . "config-dev.php";
+        $config_test = self::DIRECTORY . "config-test.php";
+        
+
+        if (file_exists($config_file))
+        {
+            array_merge(
+                $this->config,
+                require_once $config_file
+            );
+        }
+
+        if (file_exists($config_dev))
+        {
+            array_merge(
+                $this->config,
+                require_once $config_dev
+            );
+        }
+
+        if (file_exists($config_test))
+        {
+            array_merge(
+                $this->config,
+                require_once $config_test
+            );
+        }
+    }
+
+    public function getConfig()
+    {
+        return $this->config;
+    }
+}
--- src/Core.php
+++ src/Core.php
@@ -6,9 +6,9 @@ use Exception;
 
 class Core
 {
-	const APP_DIR = './../app/';
-	// const SRC_DIR = './../src/';
-    // const WEB_DIR = './../web/';
+	const DIRECTORY_APP = './../app/';
+	const DIRECTORY_SRC = './../src/';
+    const DIRECTORY_ROOT = './../public/';
     
 
 
@@ -17,54 +17,25 @@ class Core
      *
      * @var array
      */
-    private $config = [];
+    private $config;
 
     public function __construct()
     {
-        echo "App Construct<br>";
-        echo self::APP_DIR."<br>";
-
-		// Config directory
-		$dir = self::APP_DIR.'config/';
-        echo $dir."<br>";
+        $this->config = new \Dragon\Component\Config;
 
 
+        // echo "App Construct<br>";
+        // echo self::APP_DIR."<br>";
 
-		// Config files
-		$config_file = $dir.'config.php';
-		$config_dev  = $dir.'config-dev.php';
-        $config_test = $dir.'config-test.php';
-        
 
-        if (file_exists($config_file))
-        {
-            array_merge(
-                $this->config,
-                require_once $config_file
-            );
-        }
 
-        if (file_exists($config_dev))
-        {
-            array_merge(
-                $this->config,
-                require_once $config_dev
-            );
-        }
 
-        if (file_exists($config_test))
-        {
-            array_merge(
-                $this->config,
-                require_once $config_test
-            );
-        }
     }
 
 
     public function getConfig()
     {
-        return $this->config;
+        return $this->config->getConfig();
     }
 
     public function run()
