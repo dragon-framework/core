b5404d505bd98fee77c36ff5a7cb6d3d8c2e70d3
Arnaud BODEL <contact@osw3.net>
2020-03-25 00:32:02 +0100

    -

--- src/Core.php
+++ src/Core.php
@@ -4,6 +4,12 @@ namespace Dragon;
 
 class Core
 {
+	const APP_DIR = './../app/';
+	const SRC_DIR = './../src/';
+    const WEB_DIR = './../web/';
+    
+
+    
     /**
      * The App config
      *
@@ -13,7 +19,24 @@ class Core
 
     public function __construct()
     {
-        echo "App Construct";
+
+
+        echo "App Construct<br>";
+        echo self::APP_DIR."<br>";
+
+		// Config directory
+		$dir = self::APP_DIR.'config/';
+        echo $dir."<br>";
+
+
+
+		// Config files
+		$config      = $dir.'config.php';
+		$config_dev  = $dir.'config_dev.php';
+        $config_test = $dir.'config_test.php';
+        
+
+        include $config;
     }
 
     public function run()
