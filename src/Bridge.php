cf32fe4977ff60c5f8731ab29649726ab51a7898
Arnaud BODEL <contact@osw3.net>
2020-03-25 23:55:55 +0100

    -

--- src/Bridge.php
+++ src/Bridge.php
@@ -5,9 +5,16 @@ use Dragon\Component\Config\Bridge as ConfigBridge;
 
 class Bridge
 {
+    /**
+     * Config Bridge
+     *
+     * @param string|null $key
+     * @return void
+     */
     public function config(?string $key = null)
     {
         $bridge = new ConfigBridge;
+        
         return $bridge->config( $key );
     }
 }
