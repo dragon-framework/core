d4362faf913d2f092922240931b83c189445c78f
Arnaud BODEL <contact@osw3.net>
2020-03-27 01:05:03 +0100

    -

--- src/Component/Command/Command.php
+++ src/Component/Command/Command.php
@@ -1,10 +1,23 @@
 <?php
 namespace Dragon\Component\Command;
 
-class Command
+use Dragon\Kernel;
+
+class Command extends Kernel
 {
     public function __construct(array $params)
     {
-        print_r( $params );
+        unset($params[0]);
+
+        foreach ($params as $param)
+        {
+            $this->$param();
+        }
     }
+
+    public function serve()
+    {
+        exec("php -S ".$this->config('dev-host').":".$this->config('dev-port')." -t public/");
+    }
+
 }
