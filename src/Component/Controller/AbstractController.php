50e3720d331c8a9ed579494c35e052fc0d8476ac
Arnaud BODEL <contact@osw3.net>
2020-03-28 16:41:12 +0100

    -

--- src/Component/Controller/AbstractController.php
+++ src/Component/Controller/AbstractController.php
@@ -5,6 +5,11 @@ abstract class AbstractController
 {
     protected function render(string $template, array $params=[])
     {
+        // $themes_dir = DIRECTORY_THEMES;
+        $theme = getApp()->config()->getConfig('theme');
+        dump($theme);
+
+
         $loader = new \Twig\Loader\FilesystemLoader('../src/Themes');
         $twig = new \Twig\Environment($loader, [
             // 'cache' => './path/to/compilation_cache',
