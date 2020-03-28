0f55d74eba32d81a4d5f11c18a599d51c279ce0e
Arnaud BODEL <contact@osw3.net>
2020-03-28 16:52:26 +0100

    -

--- src/Component/Controller/AbstractController.php
+++ src/Component/Controller/AbstractController.php
@@ -1,16 +1,17 @@
 <?php 
 namespace Dragon\Component\Controller;
 
+use Dragon\Component\Directory\Directory;
+
 abstract class AbstractController 
 {
     protected function render(string $template, array $params=[])
     {
-        // $themes_dir = DIRECTORY_THEMES;
-        $theme = getApp()->config()->getConfig('theme');
-        dump($theme);
+        $themes_dir     = Directory::DIRECTORY_THEMES;
+        $theme_name     = getApp()->config()->getConfig('theme');
+        $theme_dir      = $themes_dir.$theme_name;
 
-
-        $loader = new \Twig\Loader\FilesystemLoader('../src/Themes');
+        $loader = new \Twig\Loader\FilesystemLoader($theme_dir);
         $twig = new \Twig\Environment($loader, [
             // 'cache' => './path/to/compilation_cache',
         ]);
