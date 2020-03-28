b07aaa59dc46ec0e0e6c8c28123bd07c2e74fa0f
Arnaud BODEL <contact@osw3.net>
2020-03-28 02:54:52 +0100

    -

--- src/Utils/dump/dump.php
+++ src/Utils/dump/dump.php
@@ -4,8 +4,8 @@ if (!function_exists('dump'))
 {
     function dump($data)
     {
-        echo "<pre>";
+		echo '<pre style="padding: 10px; font-family: Consolas, Monospace; background-color: #000; color: #FFF;">';
         print_r($data);
-        echo "</pre>";
+        echo '</pre>';
     }
 }
