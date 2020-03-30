9aadda449e878134898e615cc400964b35342211
Arnaud BODEL <contact@osw3.net>
2020-03-30 04:02:04 +0200

    -

---
+++ src/Component/Model/AbstractModel.php
@@ -0,0 +1,28 @@
+<?php 
+namespace Dragon\Component\Model;
+
+class AbstractModel
+{
+
+	/** 
+     * @var string $table Table name
+     * */
+	protected $table;
+
+	/** 
+     * @var int $primaryKey Primary key name, Default "id"
+     * */
+	protected $primaryKey = "id";
+
+	// /** 
+    //  * @var \PDO $dbh Connexion à la base de données 
+    //  * */
+    // protected $dbh;
+    
+    /**
+     * @var array $db Databases connexions
+     *
+     * @var [type]
+     */
+	protected $db;
+}
